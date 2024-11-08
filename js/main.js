8d7ee7dcc0eb41e8a549b509ee33a551e6764ab2
webQbe <webcubesolutionslk@gmail.com>
2024-11-08 19:20:25 +0530

    Add Event Listener for petForm Element

--- js/main.js
+++ js/main.js
@@ -3,3 +3,6 @@ import fetchJsonp from "fetch-jsonp";
 
  // Select Form Element
 const petForm = document.querySelector('#pet-form');
+
+// Add Event Listener for petForm Element
+petForm.addEventListener('submit', fetchAnimals);
